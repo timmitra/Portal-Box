26bc8fa6133bfa78d27e4a5ab03d82f81eaff9b7
Tim Mitra <tim@it-guy.com>
2024-09-12 11:23:15 -0400

    Initial Commit

---
+++ Portal Box/ContentView.swift
@@ -0,0 +1,34 @@
+//
+// ---------------------------- //
+// Original Project: Portal Box
+// Created on 2024-09-12 by Tim Mitra
+// Mastodon: @timmitra@mastodon.social
+// Twitter/X: timmitra@twitter.com
+// Web site: https://www.it-guy.com
+// ---------------------------- //
+// Copyright © 2024 iT Guy Technologies. All rights reserved.
+
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+
+            ToggleImmersiveSpaceButton()
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+        .environment(AppModel())
+}
---
+++ Portal Box/Portal_BoxApp.swift
@@ -0,0 +1,37 @@
+//
+// ---------------------------- //
+// Original Project: Portal Box
+// Created on 2024-09-12 by Tim Mitra
+// Mastodon: @timmitra@mastodon.social
+// Twitter/X: timmitra@twitter.com
+// Web site: https://www.it-guy.com
+// ---------------------------- //
+// Copyright © 2024 iT Guy Technologies. All rights reserved.
+
+
+import SwiftUI
+
+@main
+struct Portal_BoxApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
