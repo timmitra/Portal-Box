9aa7711985cb2a391822e88d8ddc542e4923d94f
Tim Mitra <tim@it-guy.com>
2024-09-12 17:47:53 -0400

    add portal to anchorPoint

--- Portal Box/ImmersiveView.swift
+++ Portal Box/ImmersiveView.swift
@@ -32,11 +32,42 @@ struct ImmersiveView: View {
                //box.position = [0, 1, -2] // meters
                 box.scale = [1, 2, 1]
                 
-                // make world
+                // make 4 worlds
                 let world1 = Entity()
                 world1.components.set(WorldComponent())
                 let skybox1 = await createSkyboxEntity(texture: "skybox1")
-                content.add(skybox1)
+                world1.addChild(skybox1)
+                content.add(world1)
+                
+                let world2 = Entity()
+                world1.components.set(WorldComponent())
+                let skybox2 = await createSkyboxEntity(texture: "skybox2")
+                world2.addChild(skybox2)
+                content.add(world2)
+                
+                let world3 = Entity()
+                world1.components.set(WorldComponent())
+                let skybox3 = await createSkyboxEntity(texture: "skybox3")
+                world3.addChild(skybox3)
+                content.add(world3)
+                
+                let world4 = Entity()
+                world4.components.set(WorldComponent())
+                let skybox4 = await createSkyboxEntity(texture: "skybox4")
+                world1.addChild(skybox4)
+                content.add(world4)
+                
+                // make 4 portal 1
+                let worldPortal1 = createPortal(target: world1)
+                content.add(worldPortal1)
+                
+                guard let anchorPortal1 = portalBoxScene.findEntity(named: "AnchorPortal1") else {
+                    fatalError("couldn't make anchorPortal1")
+                }
+                anchorPortal1.addChild(worldPortal1)
+                worldPortal1.transform.rotation = simd_quatf(angle: .pi/2, axis: [1,0,0])
+                
+                 
             }
         }
     }
@@ -51,6 +82,13 @@ struct ImmersiveView: View {
         entity.components.set(ModelComponent(mesh: .generateSphere(radius: 1000), materials: [material]))
         entity.scale *= .init(x: -1, y:1, z:1)
         return entity
+    }
+    
+    func createPortal(target: Entity) -> Entity {
+        let portalMesh = MeshResource.generatePlane(width: 1, depth: 1)
+        let portal = ModelEntity(mesh: portalMesh, materials: [PortalMaterial()])
+        portal.components.set(PortalComponent(target: target))
+        return portal
     }
 }
 
